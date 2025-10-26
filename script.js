4150736051f90445f567a42a09e5ff8fdc7120de
c0a3bd <40324520+c0a3bd@users.noreply.github.com>
2025-10-26 17:53:12 -0400

    p6

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xdddddd, 1);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT);
+camera.position.z = 50;
+scene.add(camera);
+
+const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x0095dd });
+const cube = new THREE.Mesh(boxGeometry, basicMaterial);
+scene.add(cube);
+cube.rotation.set(0.4, 0.2, 0);
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+render();
